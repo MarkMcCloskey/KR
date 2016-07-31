6052ae404882990fcde964bc18792717a4b878a3
Mark <mmcclosk88@gmail.com>
2016-07-31 13:12:02 -0400

    Finished exercise 1-5

---
+++ ch1/exercise1-5.c
@@ -0,0 +1,24 @@
+/* Exercise 1-5 modify the temperature conversion program to print the table in reverse order, that is, from 300 to 0. */
+#import <stdio.h>
+
+/*print Fahrenheit-Celsius table
+  for fahr= 0, 20, ..., 300 */
+
+main(){
+
+  float fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;   /* lower limit of temperature table*/
+  upper = 300; /* upper limit */
+  step = 20;   /* step size */
+
+  fahr = lower;
+  while( fahr >= lower){
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf( "%3.0f %6.1f\n", fahr, celsius);
+    fahr = fahr - step;
+  }
+   
+
+}
