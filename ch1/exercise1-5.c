ab4fc52df3f6d652f52d1552d0a4ec7b11ee434d
Mark <mmcclosk88@gmail.com>
2016-08-15 14:19:04 -0400

    Trying to organize the repo better.

--- ch1/exercise1-5.c
+++ ch1/exercise1-5.c
@@ -13,7 +13,7 @@ main(){
   upper = 300; /* upper limit */
   step = 20;   /* step size */
 
-  fahr = lower;
+  fahr = upper;
   while( fahr >= lower){
     celsius = (5.0/9.0) * (fahr-32.0);
     printf( "%3.0f %6.1f\n", fahr, celsius);
